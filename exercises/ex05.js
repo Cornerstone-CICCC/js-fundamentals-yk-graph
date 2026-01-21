8a237ed249fb55d19790b5e8976a96e88ec84c6e
yk-graph <yokokura.tatsuya@gmail.com>
2026-01-21 12:15:01 -0800

    exercise 5 completed

--- exercises/ex05.js
+++ exercises/ex05.js
@@ -26,3 +26,30 @@ var shirtSleeve = 8.47; // size M (medium)
 Then print N/A to the console because the measurements don't all match up with one particular size.
 
 */
+
+const shirtWidth = 28;
+const shirtLength = 34;
+const shirtSleeve = 10.13;
+
+switch (true) {
+  case (shirtWidth >= 18 && shirtWidth < 20) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve >= 8.13 && shirtSleeve < 8.38):
+    console.log("S");
+    break;
+  case (shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve < 8.63):
+    console.log("M");
+    break;
+  case (shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >= 30 && shirtLength < 31) && (shirtSleeve >= 8.63 && shirtSleeve < 8.88):
+    console.log("L");
+    break;
+  case (shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >= 31 && shirtLength < 33) && (shirtSleeve >= 8.88 && shirtSleeve < 9.63):
+    console.log("XL");
+    break;
+  case (shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >= 33 && shirtLength < 34) && (shirtSleeve >= 9.63 && shirtSleeve < 10.13):
+    console.log("2XL");
+    break;
+  case (shirtWidth >= 28) && (shirtLength >= 34) && (shirtSleeve >= 10.13):
+    console.log("3XL");
+    break;
+  default:
+    console.log("N/A");
+}

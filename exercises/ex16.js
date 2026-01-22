96ac3bcaca1da3ea18186b1c983786192b875268
yk-graph <yokokura.tatsuya@gmail.com>
2026-01-22 10:08:46 -0800

    exercise 16 completed

--- exercises/ex16.js
+++ exercises/ex16.js
@@ -29,9 +29,19 @@ This will be the most complicated program you've written yet, so take some time 
 */
 
 function makeLine(length) {
-    var line = "";
-    for (var j = 1; j <= length; j++) {
-      line += "* "
-    }
-    return line + "\n";
+  var line = "";
+  for (var j = 1; j <= length; j++) {
+    line += "* "
+  }
+  return line + "\n";
 }
+
+function buildTriangle(width) {
+  let triangle = "";
+  for (let i = 1; i <= width; i++) {
+    triangle += makeLine(i);
+  }
+  return triangle;
+}
+
+console.log(buildTriangle(10));

58895556d6b319eb9ddeac1f8807b1369fb7fc5a
yk-graph <yokokura.tatsuya@gmail.com>
2026-01-26 11:06:13 -0800

    add: Exercise22

--- exercises/ex22.js
+++ exercises/ex22.js
@@ -11,3 +11,11 @@ Remove "Blackberry"
 Add "Yellow" and "Green"
 Add "Purple"
 */
+
+var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+
+rainbow.splice(2, 1);
+rainbow.splice(2, 0, "Yellow", "Green");
+rainbow.splice(5, 0, "Purple");
+
+console.log(rainbow);

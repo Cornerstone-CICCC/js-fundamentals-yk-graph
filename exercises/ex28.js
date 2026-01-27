0337d3333731eb72927ef33d3b8ecb8b0d6fa140
yk-graph <yokokura.tatsuya@gmail.com>
2026-01-27 11:18:53 -0800

    add: Exercise28

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,43 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+
+const facebookProfile = {
+  name: "Tatsuya Yokokura",
+  friends: 150,
+  messages: [
+    "Hello, I came from Japan.",
+    "Just had a great day!",
+    "Learning JavaScript is fun!"
+  ],
+  postMessage: function(message) {
+    this.messages.push(message);
+  },
+  deleteMessage: function(index) {
+    if (index >= 0 && index < this.messages.length) {
+      this.messages.splice(index, 1);
+    }
+  },
+  addFriend: function() {
+    this.friends += 1;
+  },
+  removeFriend: function() {
+    if (this.friends > 0) {
+      this.friends -= 1;
+    }
+  }
+};
+
+console.log(facebookProfile);
+
+facebookProfile.postMessage("Excited to learn more coding!");
+console.log(facebookProfile.messages);
+
+facebookProfile.deleteMessage(1);
+console.log(facebookProfile.messages);
+
+facebookProfile.addFriend();
+console.log(facebookProfile.friends);
+
+facebookProfile.removeFriend();
+console.log(facebookProfile.friends);
